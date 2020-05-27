742e5ed305c5d19fe7d292180702fd11db11a4e6
Jackuz95 <56324121+Jackuz95@users.noreply.github.com>
2020-05-27 00:22:59 -0500

    updated Ui_BinnyAutomation

---
+++ Pages/checkOutPage.js
@@ -0,0 +1,25 @@
+var Data = require("../TestData/data.json");
+
+// this for checkoutpage
+
+var checkOutPage=function() {
+    
+    //Element Locators
+    this.pageTitle = $('#contents_wrapper>div:nth-child(2)');
+    this.FirstName = $('#first-name');
+    this.LastName = $('#last-name');
+    this.ZipCode = $('#postal-code');
+    this.ContinueButton = $('.checkout_buttons>input');
+
+    
+    this.CheckOut=function() {
+        
+        this.FirstName.sendKeys(Data.FirstName);
+        this.LastName.sendKeys(Data.LastName);
+        this.ZipCode.sendKeys(Data.ZipCode);
+        this.ContinueButton.click();
+    }
+    
+}
+
+module.exports=new checkOutPage();
